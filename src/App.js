2de9a4eb49e843ea1ff2134180c55f6b0d327fe1
NeginSal <neginsaljooghi@gmail.com>
2021-07-23 10:55:46 +0430

    work by dynamic value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the blog";
+  const likes = 50;
+  const link = "https://www.google.com/"
+
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title}</h1>
+        <p>liked { likes } times</p>
+        <a href={ link }> Google Site</a>
 
       </div>
     </div>
