21592d9c735bd8a927e259051ba07e98804c6c33
NeginSal <neginsaljooghi@gmail.com>
2021-07-23 11:19:21 +0430

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
 
 function App() {
-  const title = "Welcome to the blog";
-  const likes = 50;
-  const link = "https://www.google.com/"
-
-
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title}</h1>
-        <p>liked { likes } times</p>
-        <a href={ link }> Google Site</a>
-
+        <h1>App Component</h1>
       </div>
     </div>
   );
