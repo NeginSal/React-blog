a1b3ac3347b7d677cb2eb3e3c14a223783ff4cbd
NeginSal <neginsaljooghi@gmail.com>
2021-07-24 12:23:52 +0430

    Click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+   const handleClick = () => {
+      console.log('hello')
+   }
+   const handleClickagain = (name ,e) => {
+      console.log('hello' + name , e.target)
+   }
     return (
         <div className="home">
            <h2> HomePage</h2>
+           <button onClick={handleClick}>Click me</button>
+           <button onClick={(e)=>handleClickagain('negin',e)}>Click me again</button>
         </div>
      );
 }
