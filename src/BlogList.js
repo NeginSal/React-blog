d8ae33b31242e6e0719e783ae361a376a2614ee9
NeginSal <neginsaljooghi@gmail.com>
2021-07-26 11:00:23 +0430

    add delete button

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,13 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
 
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
-        {blogs.map(blog => (
+            {blogs.map(blog => (
               <div className="blog-preview" key={blog.id}>
                  <h2>{ blog.title }</h2>
                  <p><p>Written by { blog.author }</p></p>
+                 <button onClick={() => handleDelete(blog.id)}>delete blog</button>
               </div>
            ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,16 @@ const Home = () => {
       { title: 'Welcome party!', body: 'Zombie ipsum...', author: 'yoshi', id: 2 },
       { title: 'Web dev top tips', body: 'Zombie ipsum...', author: 'mario', id: 3 }
    ])
+   const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);
+   }
+
+
 
     return (
         <div className="home">
-          <BlogList blogs={blogs} title="All Blogs !" />
-          <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blog !" />
+          <BlogList blogs={blogs} title="All Blogs !" handleDelete={handleDelete}/>
         </div>
      );
 }
